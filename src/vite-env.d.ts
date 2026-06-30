94b39d2d973a823957bfffc7c2375fa2d3bc7277
hm-akalin <akalin@hm.edu>
2026-06-30 21:10:37 +0200

    fixed Anon key issue for supabase

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,9 @@
-/// <reference types="vite/client" />
-
 interface ImportMetaEnv {
-    readonly VITE_SUPABASE_URL: https://fuzdrrdqdqdlzddfewfq.supabase.co
-    readonly VITE_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1emRycmRxZHFkbHpkZGZld2ZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4MzEzNDEsImV4cCI6MjA5ODQwNzM0MX0.FJz2m71K5YzXUezNNJ6F33iUMaBrXw96vw5xaDehHYE'
+    readonly VITE_SUPABASE_URL: string
+    readonly VITE_SUPABASE_ANON_KEY: string
+    readonly VITE_GOOGLE_MAPS_API_KEY: string
 }
 
 interface ImportMeta {
     readonly env: ImportMetaEnv
 }
-
