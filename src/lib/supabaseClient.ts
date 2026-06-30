46adf3c3b9cca02ef8abbd021642b80bdb2d7fa3
hm-akalin <akalin@hm.edu>
2026-06-30 21:24:56 +0200

    added anon key to github variables

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// @ts-expect-error - Vite env, TS-Modul-Konfiguration ignorieren
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-// @ts-expect-error
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {
